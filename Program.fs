5f91503b2bd827edb30b6fd9b45ad5fe5f87d135
luisquintanilla <Luis.Quintanilla@microsoft.com>
2020-04-06 12:03:10 -0400

    Initial commit

---
+++ Program.fs
@@ -0,0 +1,32 @@
+﻿open Saturn
+open Giraffe
+
+//**************************************************//
+//             HTTP HANDLERS                        //
+//**************************************************//
+let getValues =
+    [|1;2;3|]
+
+let getValue (input:int) = 
+    let values = [|1;2;3|]
+    
+    values
+    |> Array.tryFind(fun el -> el = input)
+
+//**************************************************//
+//             ROUTER DEFINITION                    //
+//**************************************************//
+let apiRouter = router {
+    get "/api/values" (getValues |> json)
+    getf "/api/values/%i" (getValue >> json)
+}
+
+//**************************************************//
+//             APP CONFIG                           //
+//**************************************************//
+let webapp = application {
+    use_router apiRouter
+    url "http://0.0.0.0:7078"
+}
+
+run webapp
